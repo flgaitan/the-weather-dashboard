416cf9b570a5c38811b5bb94b1225600cb0713cf
flgaitan <fiama_gaitan92@hotmail.com>
2022-11-07 15:44:05 -0600

    soem code added, not fully displaying how i want it to though

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 //make a variable that will be used in moment.format() to display time
+//make API key variable
 var timeDisplayEl = $('#timeclock-display');
 var weatherApiKey = "7255f5232023240c0207da364db70563";
 let startbtn = document.getElementById('submit-Btn');
@@ -8,23 +9,14 @@ function timeDisplayElement(){
   console.log(timeDisplayEl)
    timeDisplayEl.text(timeCurrent);
    //getApi();
-  //$('.submit-Btn').on('click',function(){
-    //store in localstorage, declaring values by container/section/elements+ class id values(hour)
-    //var value = $(this).siblings('.description').val();
-    //var time = $(this).parent().attr('id');
-    //localStorage.setItem(time, value);
-    //console.log(time);
-    //console.log(value);
-//})
 }
 timeDisplayElement(); 
 
-
+//fetching API
 function getApi(e) {
   e.preventDefault()
   let cityName = document.getElementById('city-input').value
   console.log(cityName)
-    // fetch request gets a list of all the repos for the node.js organization
     //var requestApi = 'http://api.openweathermap.org/geo/1.0/direct?q=Chicago,Illinois,United States&limit=5&appid=7255f5232023240c0207da364db70563';
     let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7255f5232023240c0207da364db70563&units=imperial`
 
@@ -34,11 +26,13 @@ function getApi(e) {
       })
       .then(function (data) {
          console.log(data)
+         //fsetting variables to give values with temp, wind, and humidity information from the API data
+         //that need to be displayed on the page
          let tempData = data.main.temp
          let windSpeed = data.wind.speed 
          let humidityConditions = data.main.humidity 
-
-         document.getElementById('temp').innerHTML=`Temperature: ${tempData}` 
+        //displaying current day data
+         document.getElementById('temp').innerHTML=`Temperature: ${tempData}`
          document.getElementById('windy').innerHTML=`Wind:  ${windSpeed}` 
          document.getElementById('humid').innerHTML=`Humidity:  ${humidityConditions}` 
          forecastData(cityName)
@@ -46,6 +40,7 @@ function getApi(e) {
       })
     }
 
+//4day forecast function, 4-day forecast API link
     function forecastData(city){
       let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=7255f5232023240c0207da364db70563&units=imperial`
       fetch(forecastUrl)
@@ -54,31 +49,28 @@ function getApi(e) {
           }) 
           .then (function (data){
             console.log(data)
-            list = data.list 
-           // for (let i=0; i<list.length; i+=8){
-           /* for (let i = 0; i < list.length; i+=8) {
-              let fiveDayTemp = list[i].temp;
-              let fiveDayHumid = list[i].humidity;
-              let fiveDayWind = list[i].wind; 
-              console.log(fiveDayTemp, fiveDayHumid, fiveDayWind);
+            let list = data.list 
+            let actualTime = list.dt
+            let actualDateFormat = new Date(actualTime * 1000)
+            //console.log(actualTime, actualDateFormat)
+            //for (let i=0; i<list.length; i+=8){
+              for (let i = 0; i < list.length; i+=8){
+              //console.log(i)
+              let xtime = 0
+              let useDate = new Date(list[i].dt * 1000)
+            //console.log(useDate);
+            document.querySelector(`.fpannel${xtime}Summary`).innerHTML = `<h/4>${useDate}</h4>`;
+            document.querySelector(`.fpannel${xtime}Temp`).textContent = "Temperature: " + list[i].main.temp + "\u00BF";
+            document.querySelector(`.fpannel${xtime}Wind`).textContent = "Wind: " + list[i].wind.speed + "MPH";
+            document.querySelector(`.fpannel${xtime}Humidity`).textContent = "Humidity Conditions: " + list[i].main.humidity + "%";
+         // }
 
-              document.getElementById("temp" + (i + 1)).innerHTML = `Temperature : ` + Number(fiveDayTemp).toFixed(1) + "°";
-              document.getElementById("humid" + (i + 1)).innerHTML = `Humidity : `  + Number(fiveDayHumid).toFixed(1);
-              document.getElementById("windy" + (i + 1)).innerHTML = `Wind : `  + Number(fiveDayHumid).toFixed(1);
-              }
-
-      
           //}
-          
-           // } */
+          }
+
+
           })
 
-   /* function fiveDayForecast(){
-      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-      let monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-
-    } */
 
 
 
